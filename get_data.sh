67573281f08fa28496899ae4e058cabf327ae966
Jonathan Griffiths <jgriffiths@ebi-cli-002.ebi.ac.uk>
2018-01-26 16:26:10 +0000

    Title caps off; dl script fix

--- get_data.sh
+++ get_data.sh
@@ -4,12 +4,12 @@ if [ ! -e "bcswap_supp.html" ]
 		echo "There is no bcswap_supp.html file here - are you running the script from the right place?"
 		echo "If you still want to download here, run 'touch bcswap_supp.html' and then this script again."
 	else
-		if [ ! -d "data" ]
-			then 
-			mkdir data
-		fi
+		# if [ ! -d "data" ]
+		# 	then 
+		# 	mkdir data
+		# fi
 
-		cd data
+		# cd data
 		echo "Beginning ftp"
 		wget --user=jmlabftp --password='HOBICAmeer6' ftp://ftp2.cruk.cam.ac.uk/barcode_swapping/*
 		echo "Unpacking archive"
