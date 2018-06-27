a894fd43dcf8c90f646cd9b93550db693a639ecc
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-06-27 11:29:30 +0100

    Added message about download problems

--- get_data.sh
+++ get_data.sh
@@ -1,17 +1,14 @@
-#!/usr/bin/bash
-if [ ! -e "bcswap_supp.html" ]
-	then
-		echo "There is no bcswap_supp.html file here - are you running the script from the right place?"
-		echo "If you still want to download here, run 'touch bcswap_supp.html' and then this script again."
-	else
-		# if [ ! -d "data" ]
-		# 	then 
-		# 	mkdir data
-		# fi
+echo "Programmatic download is currently broken. Please navigate to \"https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/BarcodeSwapping2017Data/\" to get the data"
 
-		# cd data
-		echo "Beginning ftp"
-		wget --user=jmlabftp --password='HOBICAmeer6' ftp://ftp2.cruk.cam.ac.uk/barcode_swapping/*
-		echo "Unpacking archive"
-		tar -xzf bcswap_data.tar.gz
-fi
+# #!/usr/bin/bash
+# if [ ! -e "bcswap_supp.html" ]
+# 	then
+# 		echo "There is no bcswap_supp.html file here - are you running the script from the right place?"
+# 		echo "If you still want to download here, run 'touch bcswap_supp.html' and then this script again."
+# 	else
+
+# 		echo "Beginning download"
+#		curl -k https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/BarcodeSwapping2017Data/raw/master/bcswap_data.tar.gz > bcswap_data.tar.gz
+# 		echo "Unpacking archive"
+# 		tar -xzf bcswap_data.tar.gz
+# fi
